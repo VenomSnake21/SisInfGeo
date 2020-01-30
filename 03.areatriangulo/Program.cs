c93a624cbf059cfadb9d916d34fc5cfa15d30ca1
VenomSnake21 <arkantos_95@hotmail.com>
2020-01-30 11:45:30 -0600

    La ultima de hoy

---
+++ 03.areatriangulo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _03.areatriangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double altura, base1, area;
+
+            Console.WriteLine("programa que calcula el area de un triangulo");
+
+            Console.WriteLine("Dame la base: ");
+            base1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Dame la altura: ");
+            altura = double.Parse(Console.ReadLine());
+
+            area = (base1 * altura) / 2;
+            
+            Console.WriteLine($"El area del triangulo es: {area}");
+            Console.ReadLine();
+        }
+    }
+}
