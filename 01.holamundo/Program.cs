4dc2829c030971f80f0035597dba17028f52c21e
VenomSnake21 <arkantos_95@hotmail.com>
2020-01-30 11:21:04 -0600

    va todo otra vez

---
+++ 01.holamundo/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _01.holamundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Mundo!!");
+	    Console.WriteLine("Bienvenido al mundo del sida");
+        }
+    }
+}
