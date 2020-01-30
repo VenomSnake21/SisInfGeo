3c4e19bd1be7a74022525cc5e9aa401015e69598
VenomSnake21 <arkantos_95@hotmail.com>
2020-01-30 11:38:45 -0600

    Otra vez

---
+++ 02.areacirculo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _02.areacirculo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            double radio, area;
+
+            Console.WriteLine("programa que calcula el area de un circulo");
+
+            Console.WriteLine("Dame el radio: ");
+            radio = double.Parse(Console.ReadLine());
+
+            area = Math.PI * Math.Pow(radio, 2);
+            
+            Console.WriteLine($"El area del circulo es: {area}");
+            Console.ReadLine();
+        }
+    }
+}
